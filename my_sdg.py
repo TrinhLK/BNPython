2c8910b0cb6e7da83677bb214a154e9c39ceab04
LE KHANH Trinh <trinh.le-khanh@inria.fr>
2023-06-13 23:16:23 +0700

    updated sdg

--- my_sdg.py
+++ my_sdg.py
@@ -1,60 +1,46 @@
 import networkx as nx
+from pyeda.inter import *
+
+# 1. Read input file
+def read_boolean_network(file_path):
+    boolean_network = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            if line.startswith('#') or line.strip() == '':
+                continue
+            node, function = line.strip().split('=')
+            boolean_network[node.strip()] = function.strip()
+    return boolean_network
 
 def compute_signed_directed_graph(abn):
     signed_directed_graph = nx.DiGraph()
 
-    for equation in abn:
-        equation = equation.strip()
-        parts = equation.split('=')
-        target = parts[0].strip()
-        function = parts[1].strip()
+    for node, function in abn.items():
+        expr_function = expr(function)
+        # Minimize the function using Espresso
+        minimized_function = espresso_exprs(expr_function)
+        expr_str = str(minimized_function[0])
+        list_vars = []
+        if node not in signed_directed_graph:
+            signed_directed_graph.add_node(node)
 
-        if target not in signed_directed_graph:
-            signed_directed_graph.add_node(target)
+        for aVar in list(minimized_function[0].inputs):
+            if str(aVar) not in signed_directed_graph:
+                signed_directed_graph.add_node(str(aVar))
 
-        temp_functions = function.replace(' AND ', ', ')
-        temp_functions = temp_functions.replace(' OR ', ', ')
-        temp_functions = temp_functions.replace(' XOR ', ', ')
-        temp_functions = temp_functions.replace(')', '')
-        temp_functions = temp_functions.replace('(', '')
-        dependencies = temp_functions.split(', ')
-
-        print ("dependency: " + str(dependencies))
-        for dependency in dependencies:
-            sign = 1
-            if 'NOT' in dependency:
-                sign = -1
-                dependency = dependency.replace('NOT ', '')
-
-            if ' AND ' in dependency:
-                and_parts = dependency.split(' AND ')
-                for part in and_parts:
-                    if part not in signed_directed_graph:
-                        signed_directed_graph.add_node(part)
-                    signed_directed_graph.add_edge(part, target, sign=sign)
-            elif ' XOR ' in dependency:
-                xor_parts = dependency.split(' XOR ')
-                for part in xor_parts:
-                    if part not in signed_directed_graph:
-                        signed_directed_graph.add_node(part)
-                    signed_directed_graph.add_edge(part, target, sign=sign)
-                    signed_directed_graph.add_edge(target, part, sign=sign)
-            else:
-                if dependency not in signed_directed_graph:
-                    signed_directed_graph.add_node(dependency)
-                signed_directed_graph.add_edge(dependency, target, sign=sign)
-
+            if "~"+str(aVar) in expr_str:
+                list_vars.append("~"+str(aVar))
+                signed_directed_graph.add_edge(str(aVar), node, sign=-1)
+            elif str(aVar) in expr_str:
+                list_vars.append(str(aVar))
+                signed_directed_graph.add_edge(str(aVar), node, sign=1)
+                
     return signed_directed_graph
 
 # Example ABN
-abn = [
-    'x1 = x1 AND x2 AND x3',
-    'x2 = x1 OR NOT x3',
-    'x3 = (x2 AND NOT x3) OR (x1 AND NOT x2 AND NOT x3) OR (x1 AND x2 AND x3)'
-]
-
+my_abn = read_boolean_network("boolean_network.txt")
 # Compute the signed directed graph
-signed_directed_graph = compute_signed_directed_graph(abn)
+signed_directed_graph = compute_signed_directed_graph(my_abn)
 
 # Print the signed directed graph
 print("Signed Directed Graph:")
